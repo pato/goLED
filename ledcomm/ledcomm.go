d026e51267020d3c9055522fd50fb0abd8e3a50a
Pato <plankenau@gmail.com>
2014-12-17 22:38:56 -0600

    added hsv colorspace transformations

--- ledcomm/ledcomm.go
+++ ledcomm/ledcomm.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/lucasb-eyer/go-colorful"
 	"github.com/tarm/goserial"
 	"io"
 	"log"
@@ -9,6 +10,11 @@ import (
 
 func setRGB(s io.ReadWriteCloser, index, r, g, b uint8) {
 	write(s, []byte{'s', r, g, b, index})
+}
+
+func setHSV(strip io.ReadWriteCloser, index uint8, h, s, v float64) {
+	c := colorful.Hsv(h, s, v)
+	setRGB(strip, index, uint8(c.R), uint8(c.G), uint8(c.B))
 }
 
 func clear(s io.ReadWriteCloser) {
@@ -36,7 +42,7 @@ func main() {
 	clear(strip)
 
 	for i := uint8(0); i < 60; i++ {
-		setRGB(strip, i, 50, 0, 0)
+		setHSV(strip, i, float64(i*6), 1, 255)
 		time.Sleep(5 * time.Millisecond)
 		flush(strip)
 		time.Sleep(10 * time.Millisecond)
