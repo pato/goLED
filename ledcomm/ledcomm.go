22a2156183c1a227e0434ddeea4aec498790c5d5
Pato <plankenau@gmail.com>
2016-07-12 10:36:53 -0500

    Added tty detection for USB

--- ledcomm/ledcomm.go
+++ ledcomm/ledcomm.go
@@ -62,6 +62,7 @@ func ttyName() string {
 
 	for _, file := range dev {
 		if strings.Contains(file.Name(), "ttyACM") ||
+			strings.Contains(file.Name(), "ttyUSB") ||
 			strings.Contains(file.Name(), "tty.usbmodem") ||
 			strings.Contains(file.Name(), "tty.usbserial") {
 			return "/dev/" + file.Name()
